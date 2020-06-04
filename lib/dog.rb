03ef129961b2803f2719191a9fb50ebe0f054ee1
alexpac27 <alexpac27@gmail.com>
2020-06-03 22:39:09 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.map {|puppy| puts puppy.name}
+    end
+
+    def save
+        @@all << self
+    end
+
+end
+
+pluto = Dog.new("Pluto")
+fido = Dog.new("Fido")
+maddy = Dog.new("Maddy")
+# binding.pry
